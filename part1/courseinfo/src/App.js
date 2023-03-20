37a8414355af2b15b49f6ae59adf5c1f76b338c4
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-03-20 22:16:50 +0100

    finished exc. 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -30,27 +30,29 @@ const Part = (props) =>{
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content parts_c={parts} />
-      <Total parts_t={parts} />
+      <Header course={course.name} />
+      <Content parts_c={course.parts} />
+      <Total parts_t={course.parts} />
     </>
   )
 }
