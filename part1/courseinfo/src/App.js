fbb7b2425dd46e887c4988758903d5be1a98e353
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-03-20 22:11:54 +0100

    finished exc. 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,9 +7,9 @@ const Header = (props) => (
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1c} />
-      <Part part={props.part2c} />
-      <Part part={props.part3c} />
+      <Part part_p={props.parts_c[0]} />
+      <Part part_p={props.parts_c[1]} />
+      <Part part_p={props.parts_c[2]} />
     </>
   )
 }
@@ -17,14 +17,14 @@ const Content = (props) => {
 
 const Total = (props) => {
   return (    
-      <p>Number of exercises {props.part1t.exercises + props.part2t.exercises + props.part3t.exercises}</p>
+      <p>Number of exercises {props.parts_t[0].exercises + props.parts_t[1].exercises + props.parts_t[2].exercises}</p>
   )
 }
 
 
 const Part = (props) =>{
   return (
-    <p>{props.part.name} {props.part.exercises}</p>
+    <p>{props.part_p.name} {props.part_p.exercises}</p>
   )
 }
 
@@ -49,8 +49,8 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part1c={parts[0]} part2c={parts[1]} part3c={parts[2]} />
-      <Total part1t={parts[0]} part2t={parts[1]} part3t={parts[2]} />
+      <Content parts_c={parts} />
+      <Total parts_t={parts} />
     </>
   )
 }
