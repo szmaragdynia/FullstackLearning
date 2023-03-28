337545e52bce913463f8ef49a3578df1ec1c5168
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-03-28 07:20:30 +0200

    finished ex 1.7, the bloody if is so ugly

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 
 const Display = (props) => {
+  if (props.total === 0) {
+    return (
+      <>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p>all {props.total}</p>
+        <p>average 0</p>
+        <p>positive 0%</p>
+      </>
+    )
+  }
   return (
     <>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {(props.good - props.bad)/props.total }</p>
+      <p>positive {props.good*100/props.total}%</p>
     </>
-  )
+  )  
 }
 
 const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
@@ -18,10 +33,23 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const handleClickGood = () => setGood(good + 1)
-  const handleClickNeutral = () => setNeutral(neutral + 1)
-  const handleClickBad = () => setBad(bad + 1)
+  const handleClickGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+  }
+  const handleClickNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
+  }
+  const handleClickBad = () =>{
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+  } 
 
   return (
     <div>
@@ -30,7 +58,7 @@ const App = () => {
       <Button handleClick={handleClickNeutral} text='neutral' />
       <Button handleClick={handleClickBad} text='bad' />
       <h2>statistics</h2>
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Display good={good} neutral={neutral} bad={bad} total={total} />
     </div>
   )
 }
