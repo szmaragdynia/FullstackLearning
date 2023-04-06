850af48787a0f413947d4b671fff1d41a7f4cf1f
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-04-06 10:39:30 +0200

    finished learning from part2a

---
+++ part2/0functProgrBasics.js
@@ -0,0 +1,27 @@
+
+//filter
+//reject [same as filter but inverted?]
+//find (like filter, but returns first, not all)
+
+//map
+
+//reduce
+
+//mialem niby ten caly kursik od niego obejrzec bo "at least 3", ale kurwa, on tak powoli gada, poza tym ogarne all a potem zapomne instant, bo taka mam glowe. Jedyny sens to uzywac tego w czyms,bo wtdy jak zapomne to sie okaze ze i tak moge uzywac jakos skąds z glowy. Wiec obejrze tylko te 3 a potem pierdole, bede patrzyl do docsów na biezaco.DO PRZODUUU!!
+
+//first watch (read), then do! I tak bedzie.
+    //jednak nei bo czytam tak na pałe i wybiórczo, zasrane oczy/adhd. Musze przepisac, wtedy to sie streamlinuje jakos. Grrr...
+        //a moze jednak, hm?
+        //a owszem!!! A CO WIĘCEJ, TO TEMPO MI SIĘ PODOBA!!!
+            //I wgl malo jest sensu w tym co robilem, a mianowicie w komentowaniu rzeczami z kursu przykaldow kodu i trzymaniu tego. I tak w razie potrzeby szukam w kursie. Hierarhia folderow jest dziwna i ciezko sie przeklikiwac, nie wiem co gdzie jest itd. Pewnie jakbym w kursie nie znalazl to bym szukal...ale w sumie chyba bylo tak raz a i tak pierwsze to google a potem chatGPT. xD Wiec elo, jedna rzecz wiecej z glowy i szybciej sie ucze.
+                //zasadniczo mam wrazenie ze nie wiem co sie dzieje, wiec jednak musze kod napisac. ale to jest spoko, bo juz nie odkrywam linijka za lininjka kursu, a mam przecyzatne, z grubsza czaje, i teraz znacznie szybciej ogarne pisanie tego i rozumienie, niz gdy robie to "na biezaco". To jest dobry workflow.
+
+//"Try to see what happens if the braces are removed." 
+//on jakos interanlly robi to "key"??
+
+const obj = { a: 1, b: 2 };
+const { z, x } = obj; //error, names ARE important, lol
+console.log(a,b)
+// is equivalent to:
+// const a = obj.a;
+// const b = obj.b;
