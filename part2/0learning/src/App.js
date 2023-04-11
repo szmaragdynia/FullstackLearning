430e35a5c65b0abff16d07cdff97642c76baaa76
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-04-11 16:01:58 +0200

    finished ex

--- part2/0learning/src/App.js
+++ part2/0learning/src/App.js
@@ -1,7 +1,14 @@
 import Note from './components/Note'
+import { useState } from 'react'
 
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
 
-const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
@@ -10,37 +17,13 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
 
 export default App
 
-//Note that your components won’t receive key as a prop. It’s only used as a hint by React itself. If your component needs an ID, you have to pass it as a separate prop: <Profile key={id} userId={id} />.
-//source: https://react.dev/learn/rendering-lists#keeping-list-items-in-order-with-key
-  //Look, either A:
-    /*
-    <ul>
-      {notes.map(note => 
-        <li key={note.id}>
-          {note.content}
-        </li>
-      )}
-    </ul>
-    */
-  
-  //or B:
-  /*
-  const Note = ({ note }) => {
-    return (
-      <li> {note.content} </li>
-    )
-  }
-
-  const App = ....
-    ....
-    <ul>
-    {notes.map(note => 
-      <Note key={note.id} note={note} />
-    )}
-  */
