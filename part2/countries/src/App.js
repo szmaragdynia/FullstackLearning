f66eccd5d5b96b8920a372f942bd0a85be0713fb
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-04-20 01:02:47 +0200

    finished ex 2.20*

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,20 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-//todo in future?
-//notification component
-//more proper handling changing views, instead of passing the setSetCountries through everything 
+const openweathermap_api_key = process.env.REACT_APP_API_KEY
+//BEWARE, API IS STILL EMBEDED INTO FILES. SHOULD I EVER HOST THIS WEBSITE, THE API WILL BE EXPOSED
 
 
-const HandleCountriesDisplay = ({ countries, setSetCountries }) => {
+const HandleDisplay = ({ countries, setSetCountries }) => {
   if (countries.length===0) { return } 
-  
   else if (countries.length === 1) { return <ShowCountryInfo country={countries[0]} /> }
-
   else if (countries.length > 10 ) { return <p>{`Too many matches (${countries.length}), change filter`}</p> }
-  
-  else if (countries.length > 1 && countries.length <= 10 ) { return <ShowCountryList countries={countries} setSetCountries={setSetCountries} /> }
-  
+  else if (countries.length > 1 && countries.length <= 10 ) { return <ShowListOfCountries countries={countries} setSetCountries={setSetCountries} /> }
   else console.log("something is wrong")
 }
 
-const ShowCountryEntry = ({ setSetCountries, thisCountry }) => {
-  console.log("ShowCountryEntry")
+const ShowEntryInList = ({ setSetCountries, thisCountry }) => {
+  console.log("ShowEntryInList")
   const paragraphStyle = {
     marginTop: 5,
     marginBottom: 5
@@ -34,10 +29,10 @@ const ShowCountryEntry = ({ setSetCountries, thisCountry }) => {
   )
 }
 
-const ShowCountryList = ({ countries, setSetCountries }) => {
-  console.log("ShowCountryList, n of countries:",countries.length)
+const ShowListOfCountries = ({ countries, setSetCountries }) => {
+  console.log("ShowListOfCountries, n of countries:",countries.length)
   return (
-    countries.map( (country, i) => <ShowCountryEntry key={i} thisCountry={country} setSetCountries={setSetCountries}/>)
+    countries.map( (country, i) => <ShowEntryInList key={i} thisCountry={country} setSetCountries={setSetCountries}/>)
   )
 }
 
@@ -57,11 +52,38 @@ const ShowCountryInfo = ({ country }) => {
       <h3>Flag:</h3>
         {/*<img src={Object.values(country.flags)[0]} alt={country.flags.alt}/> */}
         <img src={country.flags.png} alt={country.flags.alt}/> {/*error prone because I am not sure that there is always 'png' and 'alt' field. */}
+      
+      <h3>Weather in {country.capital}</h3>
+        <RenderWeather where={country.capital} />
     </div>
     )
   }
 
+const RenderWeather = ({ where }) => {
+  const [temperature, setTemperature] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [icon, setIcon] = useState(null);
 
+
+  useEffect(() => {
+    axios
+      .get(`http://api.openweathermap.org/data/2.5/weather?q=${where}&units=metric&limit=1&appid=${openweathermap_api_key}`)
+      .then(response => {
+        setTemperature(response.data.main.temp)
+        setWind(response.data.wind.speed)
+        setIcon(response.data.weather[0].icon)
+        console.log(response.data.weather[0].icon)
+      })
+    }, [])
+
+  return (
+    <div>
+      <p> Temperature: {temperature} {'\u00B0'}C </p> 
+      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
+      <p> Wind: {wind} m/s</p> 
+    </div>
+  )
+}
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('')
@@ -93,7 +115,7 @@ function App() {
   return (
     <div>
       <p>find countries: <input onChange={handleSearchQuery} /></p>
-      <HandleCountriesDisplay countries={countries} setSetCountries={setSetCountries}/>
+      <HandleDisplay countries={countries} setSetCountries={setSetCountries}/>
     </div>
   );
 }
