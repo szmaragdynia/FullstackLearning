73f4cb4895e32811fb9251e00afdfdce350be131
Szmaragdynia <szmaragdynia@gmail.com>
2023-05-11 01:41:01 +0200

    finished ex 3.15

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,6 +3,7 @@ const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require ('cors')
+const { nextTick } = require('process')
 
 const app = express()
 
@@ -36,33 +37,6 @@ app.use(morgan(tinyPlusPOSTData))
 
 
 app.use(cors())
-
-
-
-
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 
 
@@ -103,15 +77,14 @@ app.get('/info', (req,res) => {
 
 
 app.delete('/api/persons/:id', (req,res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => res.status(204).end())
+        .catch(error => {
+            console.log("error at delete", error)
+            res.status(400).send({error: 'error at delete'})
+        })
 })
 
-const generateId = () => {
-    const max = 1000000 //magical number
-    return Math.floor((Math.random() * max))
-}
 
 app.post('/api/persons', (req,res) =>{
     
@@ -119,11 +92,6 @@ app.post('/api/persons', (req,res) =>{
         return res.status(400).json({error: 'name and number are both required'})
     }
     
-    /*const alreadyExists = persons.find(p => p.name === req.body.name)
-    if(alreadyExists) {
-        return res.status(400).json({error: 'name already in database'})
-    } */
-
     const person = new Person({
         name: req.body.name,//request.body.name?
         number: req.body.number //if number is empty, will it break or just give no number? I think that's (latter) we had the frontend
