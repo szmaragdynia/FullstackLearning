333cbb61870e9811cad5f541fcf81d6ad3116f54
Szmaragdynia <szmaragdynia@gmail.com>
2023-05-11 07:10:27 +0200

    finished ex 3.22

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,35 +3,34 @@ const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require ('cors')
-const { nextTick } = require('process')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static('build'))
 
-morgan.token('data', function (req, res) { 
-    return JSON.stringify(req.body) 
+morgan.token('data', function (req, res) {
+  return JSON.stringify(req.body)
 })
 const tinyPlusPOSTData = (tokens, req, res) => {
-    if(tokens.method(req, res) === 'POST') {
-        return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        tokens.data(req,res)
-        ].join(' ')
-    } else {
-        return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        ].join(' ')
-    }
+  if(tokens.method(req, res) === 'POST') {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.data(req,res)
+    ].join(' ')
+  } else {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+    ].join(' ')
+  }
 }
 app.use(morgan(tinyPlusPOSTData))
 
@@ -41,85 +40,85 @@ app.use(cors())
 
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World - you probably should see react app instead of this.</h1>')
+  res.send('<h1>Hello World - you probably should see react app instead of this.</h1>')
 })
 
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
-        .then(persons => res.json(persons))
-        .catch(error => next(error))
+  Person.find({})
+    .then(persons => res.json(persons))
+    .catch(error => next(error))
 })
 
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if(person){
-                res.json(person)
-            } else {
-                res.status(404).end() //id of non-existent note (but properly formed - if bad, it is caught by catch)
-            }    
-        })
-        .catch(error => next(error))    
+  Person.findById(req.params.id)
+    .then(person => {
+      if(person){
+        res.json(person)
+      } else {
+        res.status(404).end() //id of non-existent note (but properly formed - if bad, it is caught by catch)
+      }
+    })
+    .catch(error => next(error))
 })
 
 
 app.get('/info', (req, res) => {
-    const date = new Date(Date.now())
-    Person.countDocuments().then(number => {
-        res.send(`
+  const date = new Date(Date.now())
+  Person.countDocuments().then(number => {
+    res.send(`
             <p>Phonebook has info for ${number} people</p>
             <p>${date}</p>
         `)
-    })
-    
+  })
+
 })
 
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => res.status(204).end())
-        .catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => res.status(204).end())
+    .catch(error => next(error))
 })
 
 
-app.post('/api/persons', (req, res, next) =>{
-    const person = new Person({
-        name: req.body.name,//request.body.name?
-        number: req.body.number //if number is empty, will it break or just give no number? I think that's (latter) we had the frontend
-    })
+app.post('/api/persons', (req, res, next) => {
+  const person = new Person({
+    name: req.body.name,//request.body.name?
+    number: req.body.number //if number is empty, will it break or just give no number? I think that's (latter) we had the frontend
+  })
 
-    person.save()
-        .then(savedPerson => {
-            console.log(`Added ${savedPerson}`)
-            res.json(savedPerson)
-        })
-        .catch(error => next(error))
+  person.save()
+    .then(savedPerson => {
+      console.log(`Added ${savedPerson}`)
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const {name, number} = req.body
+  const { name, number } = req.body
 
-    Person.findByIdAndUpdate(
-        req.params.id, 
-        {name, number},
-        {new: true, runValidators: true, context: 'query'}
-    )
-        .then(updatedNote => res.json(updatedNote))
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => res.json(updatedNote))
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, req, res, next) => {
-    console.log("error message:", error.message)
+  console.log('error message:', error.message)
 
-    if(error.name === 'CastError') {
-        return res.status(400).send({error: 'malformed id'})
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({error: error.message})
-    }
-    next(error) //if error is not CastError
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error) //if error is not CastError
 }
 
 app.use(errorHandler)
@@ -127,5 +126,5 @@ app.use(errorHandler)
 //====================================================================================================
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -10,40 +10,40 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
-    .then(result => {
-        console.log("Successfully connted to MongoDB - phonebook")
-    })
-    .catch(error => {
-        console.log(`Error connecting to MongoDB-phonebook: ${error.message}`)
-    })
+  .then(result => {
+    console.log('Successfully connted to MongoDB - phonebook')
+  })
+  .catch(error => {
+    console.log(`Error connecting to MongoDB-phonebook: ${error.message}`)
+  })
 
 
-const validator = (val) => /^\d{2,3}-\d+$/.test(val)    
-const customValidator = [validator, "The number must be in format: 00-000..(infinite numbers) or 000-000..(infinite numbers)"]
+const validator = (val) => /^\d{2,3}-\d+$/.test(val)
+const customValidator = [validator, 'The number must be in format: 00-000..(infinite numbers) or 000-000..(infinite numbers)']
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8, //assuming its the entire string length. This does not make sense, but the information to use custom validator for the second part of the task (2/3 numbers, pause, numbers), causes me to think this is what is required (otherwise I would have to use custom validator here too probably)
-        validate: customValidator
-    }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8, //assuming its the entire string length. This does not make sense, but the information to use custom validator for the second part of the task (2/3 numbers, pause, numbers), causes me to think this is what is required (otherwise I would have to use custom validator here too probably)
+    validate: customValidator
+  }
 })
 
 personSchema.set('toJSON',{
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 
-module.exports = mongoose.model('Person', personSchema) 
+module.exports = mongoose.model('Person', personSchema)
 
 
 
@@ -52,7 +52,7 @@ module.exports = mongoose.model('Person', personSchema)
 
 /*if (process.argv.length === 3) {
     console.log('phonebook:')
-    Person.find({}).then(result => { 
+    Person.find({}).then(result => {
         result.forEach(person => {
             console.log(person.name, person.number)
         })
@@ -67,7 +67,7 @@ module.exports = mongoose.model('Person', personSchema)
     })
     person.save().then(result => {
         console.log(`added ${result.name} number ${result.number} to phonebook`)
-        mongoose.connection.close() 
+        mongoose.connection.close()
     })
 } else {console.log("unexpected error")}
 
