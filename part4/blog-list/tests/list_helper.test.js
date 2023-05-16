38f06b2c2074d3940bee549be562ed3fb5473bc3
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-05-16 14:26:47 +0200

    finished working on ex 4.6* without lodash library

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -6,6 +6,7 @@ test('dummy returns one', () => {
   expect(listHelper.dummy(blogs)).toBe(1)
 })
 
+//========================================================================================================================================================
 describe('total likes', () => {
   const listWithOneBlog = [
     {
@@ -82,7 +83,8 @@ describe('total likes', () => {
   })
 })
 
-describe.only('most likes', () => {
+//========================================================================================================================================================
+describe('most likes', () => {
   const listWithOneBlog = [
     {
       _id: '5a422aa71b54a676234d17f8',
@@ -154,7 +156,7 @@ describe.only('most likes', () => {
     author: "Edsger W. Dijkstra",
     likes: 12,
   }
-  
+
   const blogsWithManyFavorites = [
     {
       _id: "5a422a851b54a676234d17f7",
@@ -225,5 +227,113 @@ describe.only('most likes', () => {
 
   test('of bigger list with many favorites is calculated right', () => {
     expect(listHelper.favoriteBlog(blogsWithManyFavorites)).toEqual(firstMostLikes)
+  })
+})
+
+//========================================================================================================================================================
+describe('most blog posts', () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+  const topAuthor = {
+    author: "Robert C. Martin",
+    blogs: 3
+  }
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const onlyAuthor = {
+    author: "Edsger W. Dijkstra",
+    blogs: 1
+  }
+
+  const listWithTwoBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is empty object', () => {
+    expect(listHelper.mostBlogs([])).toEqual({})
+  })
+
+  test('when list has only one blog, returns that blog', () => {
+    expect(listHelper.mostBlogs(listWithOneBlog)).toEqual(onlyAuthor) //expect to equal the only object in the array
+  }) 
+
+  test('from the big list', () => {
+    expect(listHelper.mostBlogs(blogs)).toEqual(topAuthor)
+  })
+
+  test.only('of bigger list with authors with the same number of blogs is calculated right', () => {
+    expect(listHelper.mostBlogs(listWithTwoBlogs)).toEqual(onlyAuthor)
   })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -23,4 +23,67 @@ const favoriteBlog = (blogsArr) => {
   }
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = (blogsArr) => {
+  if (blogsArr.length === 0) return {}
+
+  getAuthorsBlogsAmount = (authorsBlogsAmount, currBlog) => { //the functional programming way would be to probably first copy the accumulator, work on that, and return that chagned copy. But this way is allowed as well as far as I understand (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce, ctrl+f ". If you decided to mutate the accumulator instead)
+    if (authorsBlogsAmount[currBlog.author]) {  // if in accum there is field with currBlog author name - e.g. (currblog = { author: 'pawel'}) - we check if accum = { pawel: <number. not interesting to us now> } exists
+      authorsBlogsAmount[currBlog.author] += 1  //if it does, then increment its value by one
+    } else authorsBlogsAmount[currBlog.author] = 1 //if does not, it's this authors first entry
+
+    return authorsBlogsAmount
+  }
+  /*  What accum will be, is basically:
+  accum = {
+    'pawel': 1,
+    'magda': 2
+  }
+  */
+  
+  /*We must use bracket notation (accum[currBlog.author]) instead of dot notation (accum.currBlog.author) because 
+  "bracket notation allows you to use a variable or an expression as the property name, while dot notation requires a literal property name"
+
+  let obj = {}; // An empty object
+  let key = "foo"; // A variable
+  obj[key] = "bar"; // obj is now {foo: "bar"}
+  obj.key = "baz"; // obj is now {foo: "bar", key: "baz"}
+  console.log(obj[key]); // This prints "bar"
+  console.log(obj.key); // This prints "baz"
+
+  */
+
+  const authorsBlogsAmount = blogsArr.reduce(getAuthorsBlogsAmount, {})
+  const maxValue = Math.max(...Object.values(authorsBlogsAmount)) //get the biggest blog number (value) among the authors(keys)
+  const maxAuthor = Object.keys(authorsBlogsAmount).find(author => authorsBlogsAmount[author] === maxValue) //among the keys, find who was the author(key) with the maxValue
+
+  return {author: maxAuthor, blogs: maxValue}
+
+  /* Bing proposed this:
+  // Assuming your array of objects is called books
+  function getMostFrequentAuthor(books) {
+  // Create a hashmap that counts the occurrences of each author
+  let counts = books.reduce((acc, book) => {
+    acc[book.author] = (acc[book.author] || 0) + 1;
+    // (acc[book.author] || 0) + 1. This expression means that if acc[book.author] already exists, it adds one to its value. If it does not exist, it assigns it a value of zero and then adds one to it. 
+    return acc;
+  }, {});
+
+  // Convert the hashmap to an array of [author, count] pairs
+  let entries = Object.entries(counts);
+
+  // Sort the array by count in descending order
+  entries.sort((a, b) => b[1] - a[1]);
+    //The minus sign here is not a minus sign, but a subtraction operator. It is used to compare the values of the second elements of the [key, value] pairs in the entries array. The sort method takes a comparison function that returns a positive number, zero, or a negative number depending on whether the first argument is greater than, equal to, or less than the second argument. By subtracting a[1] from b[1], we are sorting the array in descending order of the values. For example:
+    //If b[1] is 5 and a[1] is 3, then b[1] - a[1] is 2, which is positive. This means that b should come before a in the sorted array.
+    //If b[1] is 3 and a[1] is 3, then b[1] - a[1] is 0, which means that b and a are equal and their order does not matter.
+    //If b[1] is 3 and a[1] is 5, then b[1] - a[1] is -2, which is negative. This means that a should come before b in the sorted array.
+
+
+  // Return the first element of the sorted array, which is the most frequent author
+  return entries[0][0];
+  }
+  */
+
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
