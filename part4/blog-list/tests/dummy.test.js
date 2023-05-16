e5410be4a102d7843ed07b0f539ba87d37a2c0ba
userNameIsSzmaragdynia <szmaragdynia@gmail.com>
2023-05-16 12:25:15 +0200

    finished ex 4.5

--- part4/blog-list/tests/dummy.test.js
+++
@@ -1,7 +0,0 @@
-const listHelper = require('../utils/list_helper')
-
-test('dummy returns one', () => {
-  blogs = []
-
-  expect(listHelper.dummy(blogs)).toBe(1)
-})
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,5 +1,26 @@
-const dummy = (blogs) => { //array in
+const dummy = (blogsArr) => { 
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogsArr) => { 
+  sumAll = (accum, currVal) => accum + currVal.likes
+  
+  return blogsArr.reduce(sumAll, 0)
+}
+
+const favoriteBlog = (blogsArr) => {
+  findMostLiked = (accum, currVal) => currVal.likes > accum.likes ? currVal : accum //if many, first is returned!
+
+  if (blogsArr.length === 0){
+    return {}
+  } else {
+    const mostLiked = blogsArr.reduce(findMostLiked)
+    return {
+      title: mostLiked.title,
+      author: mostLiked.author,
+      likes: mostLiked.likes
+    }
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
